6f3b5201cdb9bc50123245952e1bdea2ebc573aa
Daniel Lindberg <daniel.lindberg@omegapoint.se>
2016-11-27 18:52:11 +0100

    Code cleanup, add comments

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/regression/MultiTimestepRegressionExample.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/recurrent/regression/MultiTimestepRegressionExample.java
@@ -2,13 +2,10 @@ package org.deeplearning4j.examples.recurrent.regression;
 
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.math3.util.DoubleArray;
 import org.datavec.api.records.reader.SequenceRecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVSequenceRecordReader;
 import org.datavec.api.split.NumberedFileInputSplit;
-import org.deeplearning4j.api.storage.StatsStorage;
 import org.deeplearning4j.datasets.datavec.SequenceRecordReaderDataSetIterator;
-import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.eval.RegressionEvaluation;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
@@ -19,9 +16,6 @@ import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
-import org.deeplearning4j.ui.api.UIServer;
-import org.deeplearning4j.ui.stats.StatsListener;
-import org.deeplearning4j.ui.storage.InMemoryStatsStorage;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -31,33 +25,23 @@ import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.ui.RefineryUtilities;
-import org.nd4j.linalg.api.buffer.DoubleBuffer;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.cpu.nativecpu.NDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
 import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.indexing.INDArrayIndex;
-import org.nd4j.linalg.indexing.NDArrayIndex;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
-import org.nd4j.linalg.string.NDArrayStrings;
-import org.nd4j.linalg.util.ArrayUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 
@@ -82,15 +66,23 @@ public class MultiTimestepRegressionExample {
 
     public static void main(String[] args) throws Exception {
 
-        List<String> rawStrings = prepareTrainAndTest();
+        //Set number of examples for training, testing, and time steps
+        int trainSize = 100;
+        int testSize = 20;
+        int numberOfTimesteps = 20;
 
-        int miniBatchSize = 20;
+        //Prepare multi time step data, see method comments for more info
+        List<String> rawStrings = prepareTrainAndTest(trainSize, testSize, numberOfTimesteps);
+
+        //Make sure miniBatchSize is divisable by trainSize and testSize,
+        //as rnnTimeStep will not accept different sized examples
+        int miniBatchSize = 10;
 
         // ----- Load the training data -----
         SequenceRecordReader trainFeatures = new CSVSequenceRecordReader();
-        trainFeatures.initialize(new NumberedFileInputSplit(featuresDirTrain.getAbsolutePath() + "/test_%d.csv", 0, 99));
+        trainFeatures.initialize(new NumberedFileInputSplit(featuresDirTrain.getAbsolutePath() + "/train_%d.csv", 0, trainSize-1));
         SequenceRecordReader trainLabels = new CSVSequenceRecordReader();
-        trainLabels.initialize(new NumberedFileInputSplit(labelsDirTrain.getAbsolutePath() + "/test_%d.csv", 0, 99));
+        trainLabels.initialize(new NumberedFileInputSplit(labelsDirTrain.getAbsolutePath() + "/train_%d.csv", 0, trainSize-1));
 
         DataSetIterator trainDataIter = new SequenceRecordReaderDataSetIterator(trainFeatures, trainLabels, miniBatchSize, -1, true, SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END);
 
@@ -104,9 +96,9 @@ public class MultiTimestepRegressionExample {
         // ----- Load the test data -----
         //Same process as for the training data.
         SequenceRecordReader testFeatures = new CSVSequenceRecordReader();
-        testFeatures.initialize(new NumberedFileInputSplit(featuresDirTest.getAbsolutePath() + "/test_%d.csv", 100, 119));
+        testFeatures.initialize(new NumberedFileInputSplit(featuresDirTest.getAbsolutePath() + "/test_%d.csv", trainSize, trainSize+testSize-1));
         SequenceRecordReader testLabels = new CSVSequenceRecordReader();
-        testLabels.initialize(new NumberedFileInputSplit(labelsDirTest.getAbsolutePath() + "/test_%d.csv", 100, 119));
+        testLabels.initialize(new NumberedFileInputSplit(labelsDirTest.getAbsolutePath() + "/test_%d.csv", trainSize, trainSize+testSize-1));
 
         DataSetIterator testDataIter = new SequenceRecordReaderDataSetIterator(testFeatures, testLabels, miniBatchSize, -1, true, SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END);
 
@@ -132,12 +124,7 @@ public class MultiTimestepRegressionExample {
         MultiLayerNetwork net = new MultiLayerNetwork(conf);
         net.init();
 
-        StatsStorage ss = new InMemoryStatsStorage();
-
-        UIServer uiServer = UIServer.getInstance();
-        uiServer.attach(ss);
-
-        net.setListeners(new ScoreIterationListener(20), new StatsListener(ss));
+        net.setListeners(new ScoreIterationListener(20));
 
         // ----- Train the network, evaluating the test set performance at each epoch -----
         int nEpochs = 50;
@@ -149,24 +136,20 @@ public class MultiTimestepRegressionExample {
 
             //Run regression evaluation on our single column input
             RegressionEvaluation evaluation = new RegressionEvaluation(1);
-            System.out.println("Epoch " + i + " complete. Starting evaluation:");
 
             //Run evaluation. This is on 25k reviews, so can take some time
             while(testDataIter.hasNext()){
                 DataSet t = testDataIter.next();
                 INDArray features = t.getFeatureMatrix();
                 INDArray lables = t.getLabels();
-                INDArray inMask = t.getFeaturesMaskArray();
-                INDArray outMask = t.getLabelsMaskArray();
-                INDArray predicted = net.output(features,false,inMask,outMask);
+                INDArray predicted = net.output(features,false);
 
-                evaluation.evalTimeSeries(lables,predicted,outMask);
+                evaluation.evalTimeSeries(lables,predicted);
             }
 
             System.out.println(evaluation.stats());
 
             testDataIter.reset();
-
         }
 
         /**
@@ -185,42 +168,40 @@ public class MultiTimestepRegressionExample {
         INDArray predicted  = net.rnnTimeStep(t.getFeatureMatrix());
         normalizer.revertLabels(predicted);
 
-        INDArray trainArray = createTrainIndArray(rawStrings);
-        INDArray testArray = createTestIndArray(rawStrings);
+        //Convert raw string data to IndArrays for plotting
+        INDArray trainArray = createIndArrayFromStringList(rawStrings, 0, trainSize);
+        INDArray testArray = createIndArrayFromStringList(rawStrings, trainSize, testSize);
 
         //Create plot with out data
         XYSeriesCollection c = new XYSeriesCollection();
         createSeries(c, trainArray, 0, "Train data");
-        createSeries(c, testArray, 99, "Actual test data");
-        createSeries(c, predicted, 99, "Predicted test data");
+        createSeries(c, testArray, trainSize-1, "Actual test data");
+        createSeries(c, predicted, trainSize-1, "Predicted test data");
 
         plotDataset(c);
 
         LOGGER.info("----- Example Complete -----");
     }
 
-    private static INDArray createTestIndArray(List<String> rawStrings) {
-        List<String> testStrings = rawStrings.subList(100,120);
-        double[] testPrim = new double[testStrings.size()];
 
-        for (int i = 0; i < testStrings.size(); i++) {
-            testPrim[i] = Double.valueOf(testStrings.get(i));
+    /**
+     * Creates an IndArray from a list of strings
+     * Used for plotting purposes
+     */
+    private static INDArray createIndArrayFromStringList(List<String> rawStrings, int startIndex, int length) {
+        List<String> stringList = rawStrings.subList(startIndex,startIndex+length);
+        double[] primitives = new double[stringList.size()];
+
+        for (int i = 0; i < stringList.size(); i++) {
+            primitives[i] = Double.valueOf(stringList.get(i));
         }
 
-        return Nd4j.create(new int[]{1,20},testPrim);
+        return Nd4j.create(new int[]{1,length},primitives);
     }
 
-    private static INDArray createTrainIndArray(List<String> rawStrings) {
-        List<String> trainStrings = rawStrings.subList(0,100);
-        double[] trainPrim = new double[trainStrings.size()];
-
-        for (int i = 0; i < trainStrings.size(); i++) {
-            trainPrim[i] = Double.valueOf(trainStrings.get(i));
-        }
-
-        return Nd4j.create(new int[]{1,100},trainPrim);
-    }
-
+    /**
+     * Used to create the different time series for ploting purposes
+     */
     private static XYSeriesCollection createSeries(XYSeriesCollection seriesCollection, INDArray data, int offset, String name) {
         int nRows = data.shape()[2];
         XYSeries series = new XYSeries(name);
@@ -266,7 +247,14 @@ public class MultiTimestepRegressionExample {
         f.setVisible(true);
     }
 
-    private static List<String> prepareTrainAndTest() throws IOException {
+    /**
+     * This method shows how you based on a CSV file can preprocess your data the structure expected for a
+     * multi time step problem. This examples uses a single column CSV as input, but the example should be easy to modify
+     * for use with a multi column input as well.
+     * @return
+     * @throws IOException
+     */
+    private static List<String> prepareTrainAndTest(int trainSize, int testSize, int numberOfTimesteps) throws IOException {
         Path rawPath = Paths.get(baseDir.getAbsolutePath() + "/passengers_raw.csv");
 
         List<String> rawStrings = Files.readAllLines(rawPath, Charset.defaultCharset());
@@ -277,21 +265,21 @@ public class MultiTimestepRegressionExample {
         FileUtils.cleanDirectory(featuresDirTest);
         FileUtils.cleanDirectory(labelsDirTest);
 
-        for (int i = 0; i < 100; i++) {
-            Path featuresPath = Paths.get(featuresDirTrain.getAbsolutePath() + "/test_" + i + ".csv");
-            Path labelsPath = Paths.get(labelsDirTrain + "/test_" + i + ".csv");
+        for (int i = 0; i < trainSize; i++) {
+            Path featuresPath = Paths.get(featuresDirTrain.getAbsolutePath() + "/train_" + i + ".csv");
+            Path labelsPath = Paths.get(labelsDirTrain + "/train_" + i + ".csv");
             int j;
-            for (j = 0; j < 20; j++) {
+            for (j = 0; j < numberOfTimesteps; j++) {
                 Files.write(featuresPath,rawStrings.get(i+j).concat(System.lineSeparator()).getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
             }
             Files.write(labelsPath,rawStrings.get(i+j).concat(System.lineSeparator()).getBytes(),StandardOpenOption.APPEND, StandardOpenOption.CREATE);
         }
 
-        for (int i = 100; i < 123; i++) {
+        for (int i = testSize; i < testSize+trainSize; i++) {
             Path featuresPath = Paths.get(featuresDirTest + "/test_" + i + ".csv");
             Path labelsPath = Paths.get(labelsDirTest + "/test_" + i + ".csv");
             int j;
-            for (j = 0; j < 20; j++) {
+            for (j = 0; j < numberOfTimesteps; j++) {
                 Files.write(featuresPath,rawStrings.get(i+j).concat(System.lineSeparator()).getBytes(),StandardOpenOption.APPEND, StandardOpenOption.CREATE);
             }
             Files.write(labelsPath,rawStrings.get(i+j).concat(System.lineSeparator()).getBytes(),StandardOpenOption.APPEND, StandardOpenOption.CREATE);
